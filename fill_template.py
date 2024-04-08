4ee2f4dd66fe1affa47e6c1cb2e7062492cf70fc
Matt Fisher <mfisher87@gmail.com>
2024-04-08 17:35:23 -0600

    Simpler config representation

--- fill_template.py
+++ fill_template.py
@@ -9,6 +9,11 @@ template = environment.get_template("template.py.j2")
 recipe_config_file = Path("meta.yaml")
 commands_file = Path("test_commands.sh")
 
+input_subkeys = {
+    "url": "beam.io.SomeTransformThatCanReadAFileFromAUrl",
+    "dataone_doi": "our_custom_transforms.DataOneDoiInput",
+}
+
 
 
 def get_commands() -> list[str]:
@@ -25,12 +30,36 @@ def get_recipe_config() -> dict:
         return yaml.safe_load(config_file)
 
 
+def get_input_constructor_and_arg(config: dict) -> tuple[type, any]:
+    acceptable_values = f"Acceptable values: {input_subkeys.keys()}"
+    if num_keys := len(config["input"].keys()) > 1:
+        raise RuntimeError(
+            f"Expected 1 sub-key for the `input` key; got {num_keys}."
+            f" {acceptable_values}"
+        )
+
+    key, val = list(config["input"].items())[0]
+
+    try:
+        clss = input_subkeys[key]
+    except KeyError as e:
+        raise RuntimeError(
+            f"Received unexecpected sub-key for `input` key: {key}"
+            f" {acceptable_values}"
+        )
+
+    return clss, val
+
+
+
 if __name__ == "__main__":
     commands = get_commands()
     config = get_recipe_config()
-    breakpoint()
+
+    input_constructor, input_constructor_arg = get_input_constructor_and_arg(config)
 
     print(template.render(
         commands=commands,
-        config=config,
+        input_constructor=input_constructor,
+        input_constructor_arg=input_constructor_arg,
     ))
